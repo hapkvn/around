ced21d9d336fbf4517495454e07ea32bd0adcd83
hapkvn <huyhoangpkvn75@gmail.com>
2026-07-07 23:18:08 +0700

    Di chuyển cơ bản cho xe

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Cài đặt di chuyển")]
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float turnSpeed = 3f;
+
+    [Header("Cài đặt bánh xe")]
+    [SerializeField] private Transform frontLeftWheel;  
+    [SerializeField] private Transform frontRightWheel;
+
+    [Header("Cài đặt Thân xe")]
+    [SerializeField] private float maxCarYaw = 15f;
+    [SerializeField] private float bodyRotationSpeed = 10f;
+
+
+    private Rigidbody rb;
+    private float horizontalInput;
+    private float verticalInput;
+    private float maxSteerAngle = 30f;
+    void Start()
+    {
+     rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+        Turn();
+        RotateCar();
+    }
+    private void Movement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        
+        transform.Translate(Vector3.forward * speed * verticalInput * Time.fixedDeltaTime);
+        transform.Translate(Vector3.right * horizontalInput * turnSpeed * Time.fixedDeltaTime);
+
+    }
+
+    private void Turn()
+    {
+        float horzitalInput = Input.GetAxis("Horizontal");
+
+        float currentSteerAngle = horizontalInput * maxSteerAngle;
+        
+
+        if (frontLeftWheel != null)
+        {
+            frontLeftWheel.localRotation = Quaternion.Euler(0, currentSteerAngle, 0);
+        }
+
+        if (frontRightWheel != null)
+        {
+            frontRightWheel.localRotation = Quaternion.Euler(0, currentSteerAngle, 0);
+        }
+    }
+
+    private void RotateCar()
+    {
+        float inputHorizantal = Input.GetAxis("Horizontal");
+
+        float targetYaw = inputHorizantal * maxCarYaw;
+
+        Quaternion targetRotation = Quaternion.Euler(0, targetYaw, 0);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, bodyRotationSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/road.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class road : MonoBehaviour
+{
+    [SerializeField] private GameObject road1; 
+    [SerializeField] private GameObject road2; 
+    [SerializeField] private GameObject road3; 
+    [SerializeField] private GameObject player;
+
+    [SerializeField] private float roadLength = 40f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        updateRoad();
+    }
+
+    void updateRoad()
+    {
+       
+        if (player.transform.position.z > road2.transform.position.z + 15)
+        {
+            Vector3 newPosition = road3.transform.position;
+            newPosition.z += roadLength;
+
+            road1.transform.position = newPosition;
+
+         
+            GameObject temp = road1;
+            road1 = road2;
+            road2 = road3;
+            road3 = temp;
+        }
+    }
+}
