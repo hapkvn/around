0ea4011272a1142e7dfb37689b51d7b1b9516038
hapkvn <huyhoangpkvn75@gmail.com>
2026-07-18 23:30:38 +0700

    Kiểm tra đường

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,6 +12,13 @@ public class Player : MonoBehaviour
     [Header("Cài đặt bánh xe")]
     [SerializeField] private Transform frontLeftWheel;
     [SerializeField] private Transform frontRightWheel;
+    [SerializeField] private Transform rearLeftWheel;
+    [SerializeField] private Transform rearRightWheel;
+
+    [Header("Cài đặt mặt đất")]
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float rayLength = 0.5f;
+    public bool isCarGrounded = false;
 
     private Rigidbody rb;
     private float maxSteerAngle = 30f;    
@@ -27,9 +34,17 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        GetInput(); 
-        Movement();
-        Turn();
+        GetInput();
+
+        // Cập nhật trạng thái chạm đất liên tục
+        CheckAllWheelsGrounded();
+
+        // Chỉ cho phép xe chạy và bẻ lái khi có ít nhất 1 bánh (hoặc cả 4 bánh) chạm đất
+        if (isCarGrounded)
+        {
+            Movement();
+            Turn();
+        }
     }
 
     private void GetInput()
@@ -68,6 +83,36 @@ public class Player : MonoBehaviour
         {
             frontRightWheel.localRotation = Quaternion.Lerp(frontRightWheel.localRotation, targetWheelRotation, turnSpeed * Time.fixedDeltaTime);
         }
+    }
+    private void CheckAllWheelsGrounded()
+    {
+        // Kiểm tra từng bánh xem có chạm đất không
+        bool isFLGrounded = CheckSingleWheel(frontLeftWheel);
+        bool isFRGrounded = CheckSingleWheel(frontRightWheel);
+        bool isRLGrounded = CheckSingleWheel(rearLeftWheel);
+        bool isRRGrounded = CheckSingleWheel(rearRightWheel);
+
+        // Xe được tính là chạm đất nếu ÍT NHẤT 1 trong 4 bánh chạm đất
+        // (Nếu bạn muốn khắt khe hơn, bắt buộc cả 4 bánh chạm đất mới được chạy thì thay dấu || thành &&)
+        isCarGrounded = isFLGrounded || isFRGrounded || isRLGrounded || isRRGrounded;
+    }
+
+    // Hàm bắn tia Raycast cho 1 bánh xe
+    private bool CheckSingleWheel(Transform wheel)
+    {
+        if (wheel == null) return false;
+
+        // Bắn tia từ vị trí bánh xe, hướng thẳng xuống (-transform.up)
+        return Physics.Raycast(wheel.position, -transform.up, rayLength, groundLayer);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red; // Đổi màu tia thành đỏ
+
+        if (frontLeftWheel != null) Gizmos.DrawLine(frontLeftWheel.position, frontLeftWheel.position - transform.up * rayLength);
+        if (frontRightWheel != null) Gizmos.DrawLine(frontRightWheel.position, frontRightWheel.position - transform.up * rayLength);
+        if (rearLeftWheel != null) Gizmos.DrawLine(rearLeftWheel.position, rearLeftWheel.position - transform.up * rayLength);
+        if (rearRightWheel != null) Gizmos.DrawLine(rearRightWheel.position, rearRightWheel.position - transform.up * rayLength);
     }
 
     public void UpPoiterLeft() { isMoveLeft = false; }
---
+++ Assets/Scripts/obstacle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class obstacle : MonoBehaviour
+{
+    
+    void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("player"))
+        {
+            Debug.Log("Va cham");
+            // Thực hiện hành động khi va chạm với Player
+        }
+    }
+}
